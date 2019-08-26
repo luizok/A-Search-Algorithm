815d2ca7adb1791b852d258fd3288bbc5366bc9a
Luiz Felipe <luizfelipesmaciel@gmail.com>
2019-08-25 23:40:45 -0300

    Created some tests using MinHeap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from n_puzzle import NPuzzleInstance
+from min_heap import MinHeap
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -6,5 +8,18 @@ if __name__ == '__main__':
     instance = NPuzzleInstance(3)
     print(str(instance))
     instance.calculate_distance()
+
+    heap = MinHeap()
+    n = 20
+    itens = np.random.randint(10, 100, (n, 1))
+    priors = np.random.randint(1, 15, (n, 1))
+
+    for item, prior in zip(itens, priors):
+        heap.push(item[0], prior[0])
+
+    print(heap)
+    print(heap.pop())
+    print(heap)
+
 
     
