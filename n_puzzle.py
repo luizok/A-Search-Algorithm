717e21eeb0c4e68a31a67e5a548d214acfc8ed61
Luiz Felipe <luizfelipesmaciel@gmail.com>
2019-08-29 19:24:05 -0300

    refactored NPuzzleInstance to use properties from State

--- n_puzzle.py
+++ n_puzzle.py
@@ -28,15 +28,14 @@ class NPuzzleInstance:
         np.random.shuffle(c_state)
         c_state = np.asarray(c_state).reshape((self.m, self.n))
 
-        return c_state
+        return State(c_state)
 
     def __build_goal_state(self):
         g_state = np.arange(self.m * self.n).reshape((self.m, self.n))
         g_state = np.roll(g_state, -1)
         # The line above shifts the 0 (empty space) to the last position
         # Ex.: np.roll([0, 1, 2, ..., m*n-1], -1) --> [1, 2, ..., m*n-1, 0]
-        return g_state
-
+        return State(g_state)
         # If some_state is None, set it to self.current_state
         some_state = some_state if some_state is not None else self.current_state
 
@@ -47,8 +46,7 @@ class NPuzzleInstance:
             for j in range(self.n):
                 if some_state[i, j] != self.goal_state[i, j]:
                     # total_diferents += 1
-                    g_i, g_j = np.where(self.goal_state == some_state[i, j])
-                    g_i, g_j = g_i[0], g_j[0]
+                    g_i, g_j = self.goal_state.get_indexes(some_state[i, j])
 
                     dx = abs(i - g_i)
                     dy = abs(j - g_j)
@@ -58,26 +56,23 @@ class NPuzzleInstance:
         return total_distance  # total_diferents
 
     def is_solved(self):
-        return np.array_equal(self.current_state, self.goal_state)
+        return self.current_state == self.goal_state
 
     def neighbors(self):
-        x, y = np.where(self.current_state == 0)
-        x, y = x[0], y[0]
+        x, y = self.current_state.get_indexes(0)
 
         for (i, j) in self.neighbors_indexes:
             if 0 <= x + i < self.m and 0 <= y + j < self.n:
-                    neigh = np.copy(self.current_state)
+                    neigh = State(self.current_state.matrix)
                     neigh[x + i, y + j], neigh[x, y] = neigh[x, y], neigh[x + i, y + j]
+
+                    neigh.greedy_value = self.current_state.greedy_value + 1
+                    neigh.heuristic_value = self.heuristic(neigh)
 
                     yield neigh
 
     def __str__(self):
-        shape_str = str(self.goal_state.shape)
-        i_state_str = str(self.initial_state).replace(' 0', ' _').replace('[0', '[_')
-        g_state_str = str(self.goal_state).replace(' 0', ' _').replace('[0', '[_')
 
-        fmt_str = 'Dimensions: {}\nCurrent State:\n{}\n\nGoal State:\n{}\n'.format(
-            shape_str, i_state_str, g_state_str
+        return 'Dimensions: {}\nInitial State:\n{}\n\nGoal State:\n{}\n'.format(
+            self.goal_state.shape, str(self.initial_state), str(self.goal_state)
         )
-
-        return fmt_str
